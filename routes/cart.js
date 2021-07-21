0a4e93a66e85a54f57e2f8a663837cfbfcfab2e8
Shivanshu Ahuja <shivanshuahuja@gmail.com>
2021-07-22 01:41:49 +0530

    Update Controller

--- routes/cart.js
+++ routes/cart.js
@@ -1,3 +1,9 @@
 const { Router } = require('express');
-const authController = require('../controllers/cartControllers');
+const cartController = require('../controllers/cartControllers');
 const router = Router();
+
+router.get('/cart/:id',cartController.get_cart_items);
+router.post('/cart/:id',cartController.add_cart_item);
+router.delete('/cart/:userId/:itemId',cartController.delete_item);
+
+module.exports = router;
