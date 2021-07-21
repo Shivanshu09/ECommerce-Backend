5bfe876d818ec7f957a2fe646caa46551c0b6050
Shivanshu Ahuja <shivanshuahuja@gmail.com>
2021-07-22 01:47:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
 
+const authRoutes = require('./routes/auth');
+const itemRoutes = require('./routes/item');
+const cartRoutes = require('./routes/cart');
+const orderRoutes = require('./routes/order');
+
 const app = express();
 app.use(express.json());
+
+app.use('/api',authRoutes);
+app.use('/api',itemRoutes);
+app.use('/api',cartRoutes);
+app.use('/api',orderRoutes);
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
